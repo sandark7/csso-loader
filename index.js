0a4477779caaac53fce080c3302fed4e405e68bb
an.smirnov <sandark7@gmail.com>
2017-03-09 17:53:43 +0300

    Updated loader-utils

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var csso = require('csso');
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
-  var query = loaderUtils.parseQuery(this.query);
+  var query = loaderUtils.getOptions(this) || {};
   var cssoOptions = this.options ? this.options.csso : false;
   var filename = path.basename(this.resourcePath);
   var options = {};
